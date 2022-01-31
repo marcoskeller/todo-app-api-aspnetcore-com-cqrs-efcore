e7b8f54bf5b73a46a7ecc907de51a363e4dd6bde
marcos.fonseca <marcos.fonseca@squadra.com.br>
2022-01-31 16:34:53 -0300

    Refatoracao dos testes unitarios para o CreateTodoCommand

--- Todo.Domain.Tests/CommandTests/CreateTodoCommandTests.cs
+++ Todo.Domain.Tests/CommandTests/CreateTodoCommandTests.cs
@@ -7,19 +7,23 @@ namespace Todo.Domain.Tests.CommandTests
     [TestClass]
     public class CreateTodoCommandTests
     {
+        private readonly CreateTodoCommand _invalidCommand = new CreateTodoCommand("", DateTime.Now, "");
+
+        private readonly CreateTodoCommand _validCommand = new CreateTodoCommand("Titulo da tarefa", DateTime.Now, "Marcos");
+
         [TestMethod]
         public void Dado_um_comano_invalido()
         {
             #region Arrange
-            var command = new CreateTodoCommand("", DateTime.Now, "");
+
             #endregion
 
             #region Act
-            command.Validate();
+            _invalidCommand.Validate();
             #endregion
 
             #region Assert
-            Assert.AreEqual(command.Valid, false);
+            Assert.AreEqual(_invalidCommand.Valid, false);
             #endregion
         }
 
@@ -27,15 +31,15 @@ namespace Todo.Domain.Tests.CommandTests
         public void Dado_um_comano_valido()
         {
             #region Arrange
-            var command = new CreateTodoCommand("Titulo da tarefa", DateTime.Now, "Marcos");
+
             #endregion
 
             #region Act
-            command.Validate();
+            _validCommand.Validate();
             #endregion
 
             #region Assert
-            Assert.AreEqual(command.Valid, true);
+            Assert.AreEqual(_validCommand.Valid, true);
             #endregion
         }
     }
