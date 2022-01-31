ae0ef3409c3599415962389b13c163ea8ba7d7f9
marcos.fonseca <marcos.fonseca@squadra.com.br>
2022-01-31 19:58:13 -0300

    Implementacao dos testes unitarios para o CreateTodoHandlerTests

---
+++ Todo.Domain.Tests/HandlerTests/CreateTodoHandlerTests.cs
@@ -0,0 +1,48 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using Todo.Domain.Commands;
+using Todo.Domain.Handlers;
+using Todo.Domain.Tests.Repositories;
+
+namespace Todo.Domain.Tests.HandlerTests
+{
+    [TestClass]
+    public class CreateTodoHandlerTests
+    {
+
+        [TestMethod]
+        public void Dado_um_comano_invalido()
+        {
+            #region Arrange
+            var command = new CreateTodoCommand("", DateTime.Now,"");
+            var handler = new TodoHandler(new FakeTodoRepository());
+
+            #endregion
+
+            #region Act
+            var result = (GenericCommandResult)handler.Handle(command);
+            #endregion
+
+            #region Assert
+            Assert.AreEqual(result.Sucess, false);
+            #endregion
+        }
+
+        [TestMethod]
+        public void Dado_um_comano_valido()
+        {
+            #region Arrange
+            var command = new CreateTodoCommand("Titulo da tarefa", DateTime.Now, "Marcos");
+            var handler = new TodoHandler(new FakeTodoRepository());
+            #endregion
+
+            #region Act
+            var result = (GenericCommandResult)handler.Handle(command);
+            #endregion
+
+            #region Assert
+            Assert.AreEqual(result.Sucess, true);
+            #endregion
+        }
+    }
+}
