8188b326995b1b3ec8bf628e5c8b8d09c7afdd49
marcos.fonseca <marcos.fonseca@squadra.com.br>
2022-01-31 13:08:51 -0300

    Criacao dos Handlers do projeto

---
+++ Todo.Domain/Handlers/TodoHandler.cs
@@ -0,0 +1,29 @@
+﻿using Flunt.Notifications;
+using Todo.Domain.Commands;
+using Todo.Domain.Commands.Contracts;
+using Todo.Domain.Handlers.Contracts;
+using Todo.Domain.Repositories;
+
+namespace Todo.Domain.Handlers
+{
+    public class TodoHandler : Notifiable, IHandler<CreateTodoCommand>, IHandler<UpdateTodoCommand>
+    {
+        private readonly ITodoRepository _repository;
+
+        public TodoHandler(ITodoRepository repository)
+        {
+            _repository = repository;
+        }
+
+        public ICommandResult Handle(CreateTodoCommand command)
+        {
+            
+            throw new System.NotImplementedException();
+        }
+
+        public ICommandResult Handle(UpdateTodoCommand command)
+        {
+            throw new System.NotImplementedException();
+        }
+    }
+}
