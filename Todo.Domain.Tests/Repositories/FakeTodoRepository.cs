4d11f72044e0812ea403f3dffc1fa20c0b8c7a69
marcos.fonseca <marcos.fonseca@squadra.com.br>
2022-02-03 15:32:28 -0300

    Modificando o FakeTodoRepository com as novas implementacoes

--- Todo.Domain.Tests/Repositories/FakeTodoRepository.cs
+++ Todo.Domain.Tests/Repositories/FakeTodoRepository.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections.Generic;
 using Todo.Domain.Entities;
 using Todo.Domain.Repositories;
 
@@ -7,7 +8,23 @@ namespace Todo.Domain.Tests.Repositories
     public class FakeTodoRepository : ITodoRepository
     {
         public void Create(TodoItem todo)
-        {          
+        {  
+            
+        }
+
+        public IEnumerable<TodoItem> GetAll(string user)
+        {
+            throw new NotImplementedException();
+        }
+
+        public IEnumerable<TodoItem> GetAllDone(string user)
+        {
+            throw new NotImplementedException();
+        }
+
+        public IEnumerable<TodoItem> GetAllUnDone(string user)
+        {
+            throw new NotImplementedException();
         }
 
         public TodoItem GetById(Guid id, string user)
@@ -15,8 +32,14 @@ namespace Todo.Domain.Tests.Repositories
             return new TodoItem("Titulo aqui", DateTime.Now, "Marcos");
         }
 
+        public IEnumerable<TodoItem> GetByPeriod(string user, DateTime date, bool done)
+        {
+            throw new NotImplementedException();
+        }
+
         public void Update(TodoItem todo)
-        {        
+        { 
+            
         }
     }
 }
